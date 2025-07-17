7d6389a9ea0ca2581de60175c4b824bc2d9f149a
anastasia.test <nastyona957@yandex.ru>
2025-07-17 13:39:52 +0300

    Rename percent to Percent

--- src/main/java/org/example/BonusService.java
+++ src/main/java/org/example/BonusService.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class BonusService {
     public long calculate(long amount, boolean registered) {
-        int percet = registered ? 3 : 1;
-        long bonus = amount * percet / 100;
+        int Percet = registered ? 3 : 1;
+        long bonus = amount * Percet / 100;
         long limit = 500;
         if(bonus > limit) {
             bonus = limit;
